1e8250f102c141eed81e15424a2056941f7a718b
Kristina <kristina@10gen.com>
2014-01-06 00:39:33 -0500

    Add more options to clustering

--- cluster.js
+++ cluster.js
@@ -1,16 +1,14 @@
 goog.provide('lib.Cluster');
 
 // Creates a cluster of things using a hub-and-spoke model.
-// @param ctor contructor This should be a constructor for a subclass of 
+// @param ctor contructor This should be a constructor for a subclass of
 //     lime.Sprite.
 lib.Cluster = function() {
-    this.MIN = 7;
-    this.MAX = 20;
+    this.max_ = 10;
     this.MIN_SPOKES = 2;
     this.MAX_SPOKES = 7;
-    this.MIN_DIST = 2*LEN;
-    this.MAX_DIST = 3*LEN;
-    this.MAX_TRIES = 3;
+    this.min_distance_ = LEN;
+    this.MAX_DIST = LEN*2;
     this.JITTER = LEN/2;
 };
 
@@ -33,19 +31,34 @@ lib.Cluster.prototype.setMap = function(map) {
     return this;
 };
 
+lib.Cluster.prototype.setTags = function(tags) {
+    this.tags_ = tags;
+    return this;
+};
+
+lib.Cluster.prototype.setNumber = function(number) {
+    this.max_ = number;
+    return this;
+};
+
+// Sets the minimum distance for objects.  Always greater than 0.
+lib.Cluster.prototype.setMinDistance = function(dist) {
+    this.min_distance_ = dist <= 0 ? 1 : dist;
+    return this;
+};
+
 lib.Cluster.prototype.generate = function() {
-    this.max_ = lib.random(this.MIN, this.MAX);
-    this.num_ = 1;
+    this.num_ = 0;
 
     var tries = 0;
-    while (tries++ < this.MAX_TRIES && this.num_ < this.max_) {
-	var hub = new goog.math.Coordinate(
-	    lib.random(this.box_.left, this.box_.right),
-	    lib.random(this.box_.top, this.box_.bottom));
+    while (this.num_ < this.max_) {
+        var hub = new goog.math.Coordinate(
+            lib.random(this.box_.left, this.box_.right),
+            lib.random(this.box_.top, this.box_.bottom));
 
-	var hub_inst = this.creator_(hub);
-
-	this.addSpokes_(hub);
+        if (this.createSpoke_(hub, 0) != null) {
+            this.addSpokes_(hub);
+        }
     }
 };
 
@@ -53,7 +66,7 @@ lib.Cluster.prototype.generate = function() {
 // @param hub goog.math.Coordinate The position of the hub.
 lib.Cluster.prototype.addSpokes_ = function(hub) {
     if (this.num_ >= this.max_) {
-	return;
+        return;
     }
 
     // Create huts as "spokes" around a hub hut.
@@ -62,28 +75,29 @@ lib.Cluster.prototype.addSpokes_ = function(hub) {
     // Start at a random offset.
     var offset = lib.random(360);
 
-    for (var i = 0; this.num_ < this.max_ && i < num_spokes; i++, offset += angle) {
-	var spoke = this.createSpoke_(hub, offset);
-	if (spoke == null) {
-	    continue;
-	}
+    for (var i = 0; this.num_ < this.max_ && i < num_spokes;
+         i++, offset += angle) {
+        var spoke = this.createSpoke_(hub, offset);
+        if (spoke == null) {
+            continue;
+        }
 
-	// Fork off a new hub?
-	if (lib.random(this.max_-this.num_) != 0) {
-	    this.addSpokes_(spoke.getPosition());
-	}
+        // Fork off a new hub?
+        if (lib.random(this.max_-this.num_) != 0) {
+            this.addSpokes_(spoke.getPosition());
+        }
     }
 };
 
 lib.Cluster.prototype.createSpoke_ = function(hub, offset) {
-    var dist = lib.random(this.MIN_DIST, this.MAX_DIST);
+    var dist = lib.random(this.min_distance_, this.MAX_DIST);
     var pos = new goog.math.Coordinate(
-	hub.x+Math.cos(goog.math.toRadians(offset))*dist+this.jitter_(),
-	hub.y+Math.sin(goog.math.toRadians(offset))*dist+this.jitter_());
+        hub.x+Math.cos(goog.math.toRadians(offset))*dist+this.jitter_(),
+        hub.y+Math.sin(goog.math.toRadians(offset))*dist+this.jitter_());
 
     // Outside of range or too close to another.
     if (!this.box_.contains(pos) || this.hasNearby_(pos)) {
-	return null;
+        return null;
     }
 
     var thing = this.creator_(pos);
@@ -97,7 +111,8 @@ lib.Cluster.prototype.jitter_ = function() {
 };
 
 lib.Cluster.prototype.hasNearby_ = function(pos) {
-    var box = new goog.math.Box(pos.y, pos.x, pos.y, pos.x).expand(this.MIN_DIST);
-    var results = this.map_.findInBox(box);
-    return results.length != 0;
+    var box = new goog.math.Box(
+        pos.y-this.min_distance_, pos.x+this.min_distance_,
+        pos.y+this.min_distance_, pos.x-this.min_distance_);
+    return this.map_.hasInBox(box, this.tags_);
 };
