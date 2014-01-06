99cfbc8c670dde444b286448dd0908e0a1f72bba
Kristina <kristina@10gen.com>
2014-01-06 00:39:14 -0500

    Spacing

--- direction.js
+++ direction.js
@@ -1,25 +1,30 @@
 goog.provide('lib.Direction');
 
 // For attaching directionality to a sprite.
+//
+// Example:
+//
+//     goog.object.extend(this, new lib.Direction(this));
+//
 lib.Direction = function(sprite) {
-    // This assumes sprite start facing right, since I tend to draw them that 
+    // This assumes sprite start facing right, since I tend to draw them that
     // way.
     this.faceRight.call(sprite);
-};
-
-lib.Direction.prototype.setRandom = function() {
-    sprite.facing_ = lib.random(2);
-    sprite.changeDirection();
 };
 
 lib.Direction.LEFT = 0;
 lib.Direction.RIGHT = 1;
 
+lib.Direction.prototype.faceRandom = function() {
+    this.facing_ = lib.random(2);
+    this.changeDirection();
+};
+
 // Switches the direction the sprite "this" is facing.
 // @return this
 lib.Direction.prototype.changeDirection = function() {
-    return (this.facing_ == lib.Direction.LEFT) ? 
-	this.faceRight() : this.faceLeft();
+    return (this.facing_ == lib.Direction.LEFT) ?
+        this.faceRight() : this.faceLeft();
 };
 
 lib.Direction.prototype.faceLeft = function() {
